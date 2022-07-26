99387effac043a1812a8510cc6aec0e15109573e
Patrick Mensah <70266683+patmensa39@users.noreply.github.com>
2022-07-26 10:41:48 -0400

    Add files via upload

---
+++ Pie chart .R
@@ -0,0 +1,15 @@
+## PIE CHART IN R PROGRAMMING ###
+#Reading the dataset 
+data <-read.csv("piedata.csv")
+View(data) #Viewing the dataset
+
+
+pie(data$amounts)
+
+#Drawing the pie chart with labels 
+pie(data$amounts, labels = as.character(data$names))
+
+#You can also change the colors 
+pie(data$amounts, labels = as.character(data$names), col = terrain.colors(6))
+
+
